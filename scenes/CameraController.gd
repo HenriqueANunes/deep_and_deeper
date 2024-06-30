825d037bb380c6fcb9a21ae7cd96c4f81f6f2e78
hman <henrique.a.nunes@outlook.com>
2024-06-30 17:30:53 -0300

    new asset and animation

---
+++ scenes/CameraController.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+var player
+
+@export var sensitivity := 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_tree().get_nodes_in_group('Player')[0]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position = player.global_position
+	$SpringArm3D/Camera3D.look_at(player.get_node('LookAt').global_position)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		var temp_rot = rotation.x - event.relative.y / 1000 * sensitivity
+		rotation.y -= event.relative.x / 1000 * sensitivity
+		
+		rotation.x = clamp(temp_rot, -1, -0.1)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,12 @@
 extends CharacterBody3D
 
-@onready var camera = $camera
-@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
-@onready var visuals = $visuals
 
-const SPEED = 3.0
+const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
-@export var sens_horizontal = 0.15
-@export var sens_vertical = 0.15
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
-func _input(event):
-	if event is InputEventMouseMotion:
-		var rad_rotation_y = deg_to_rad(-event.relative.x * sens_horizontal)
-		var rad_rotation_x = deg_to_rad(-event.relative.y * sens_vertical)
-		
-		rotate_y(rad_rotation_y)
-		visuals.rotate_y(-rad_rotation_y)
-		camera.rotate_x(rad_rotation_x)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -36,23 +19,13 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if animation_player.current_animation != "walking":
-			animation_player.play("walking")
-		print('input: ', input_dir)
-		print('position: ', position)
-		print('direction: ', direction)
-		print('position + direction: ', position + direction)
-		visuals.look_at(position+direction)
-		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:		
-		if animation_player.current_animation != "idle":
-			animation_player.play("idle")
-			
+	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
 	move_and_slide()
